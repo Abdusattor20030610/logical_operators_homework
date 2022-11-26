dfacbfd8d28c9df0f1f33657b4af8b1f51ad4592
Abdusattor10062003 <abdusattormuydinov10062003@codeschool.uz>
2022-11-27 01:37:49 +0500

    solved

--- logic10.py
+++ logic10.py
@@ -1,4 +1,5 @@
 def main(a):
+    return 10>a//10>=1
     """
     Given integer a,  check the following statement "The integer is two-digit number".
     Args:
@@ -6,4 +7,4 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+print(main(3))
