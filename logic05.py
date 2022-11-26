2f034db36161c67f839101c87e36e41b51a02b82
Abdusattor10062003 <abdusattormuydinov10062003@codeschool.uz>
2022-11-27 01:05:57 +0500

    solved

--- logic05.py
+++ logic05.py
@@ -1,4 +1,7 @@
 def main(a,b):
+    a=a%2==1
+    b=b%2==1
+    return a and b
     """
     Given two integers a, b,  check the following statement "Each of the numbers 'a' and 'b' is odd".
     Args:
@@ -7,4 +10,4 @@ def main(a,b):
     Returns:
         bool: answer
     """
-    return
+print(main(3,8))
