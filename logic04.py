bab787a055aaa8180d8655f6d05c7fd0a3b2f001
Abdusattor10062003 <abdusattormuydinov10062003@codeschool.uz>
2022-11-27 01:02:02 +0500

    solved

--- logic04.py
+++ logic04.py
@@ -1,5 +1,9 @@
 def main(a,b):
-    """
+    a=a%2
+    b=b%2
+    return a==0==b
+print(main(3,6))
+"""
     Given two integers a, b,  check the following statement "Each of the numbers 'a' and 'b' is even".
     Args:
         a(int): parameter a
@@ -7,4 +11,3 @@ def main(a,b):
     Returns:
         bool: answer
     """
-    return
