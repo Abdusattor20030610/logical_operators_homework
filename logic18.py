3c44bd534e2f3b6f6a4fb78fd8f8715b1d73dfa8
Abdusattor10062003 <abdusattormuydinov10062003@codeschool.uz>
2022-11-27 02:10:31 +0500

    solved

--- logic18.py
+++ logic18.py
@@ -1,8 +1,9 @@
 def main(a):
+    return a//10000<a//1000%10<a//100%10<a//10%10
     """Given a five-digit integer a, check the following statement "All digits of the number are in descending order".
     Args:
         a(int): parameter a
     Returns:
         bool: answer
     """
-    return
+print(main(1347))
